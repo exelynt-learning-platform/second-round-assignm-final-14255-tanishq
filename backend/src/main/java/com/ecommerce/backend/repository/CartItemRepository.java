0b47adb9d24a1d7f95cf9629c8194f05132d14de
tanishq231 <77448114+tanishq231@users.noreply.github.com>
2026-04-02 05:04:58 +0530

    Delete backend directory

--- backend/src/main/java/com/ecommerce/backend/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.backend.repository;
-
-import com.ecommerce.backend.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- backend/src/main/java/com/ecommerce/backend/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.backend.repository;
-
-import com.ecommerce.backend.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
