523215946b1dc8e1b22a6de7a3227b0b27e8bc40
tanishq231 <77448114+tanishq231@users.noreply.github.com>
2026-04-02 05:13:33 +0530

    Delete backend directory

--- backend/ECommerceApp/src/main/java/com/Ecommerce/ECommerceApp/Enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.Ecommerce.ECommerceApp.Enums;
-
-public enum Role {
-
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- backend/ECommerceApp/src/main/java/com/Ecommerce/ECommerceApp/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Ecommerce.ECommerceApp.repository;
-
-import com.Ecommerce.ECommerceApp.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
